bf860c7923253dbf225f35aee9d8908f9f935733
the1stenhancer <chuyebrian10@gmail.com>
2024-05-14 21:25:36 +0100

    created Detail model for user details

--- crud/admin.py
+++ crud/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Detail
 
 # Register your models here.
+
+@admin.register(Detail)
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ["phone", "dob", "married", "kids", "doh", "title", "duration", "updated"]
+    list_filter = ["title", "doh", "duration"]
--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+
 
 # Create your models here.
+
+MARITAL_STATUS = (
+    ('m', 'Married'),
+    ('nm', 'Not Married'),
+)
+
+class Detail(models.Model):
+    # personal information
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(help_text=_("Phone number"), max_length=20, null=False)
+    dob = models.DateField(help_text=_("Date of birth"), null=False)
+    married = models.BooleanField(choices=MARITAL_STATUS, help_text=_("Marital status"), null=False)
+    kids = models.PositiveIntegerField(default=0, help_text=_("Number of kids"), null=False)
+    # work information
+    doh = models.DateField(help_text=_("Date of hiring"), null=False)
+    title = models.CharField(help_text=_("Job title"), max_length=255, null=False)
+    duration = models.PositiveIntegerField(help_text=_("Contract duration"), default=3, null=False)
+    # other information
+    created = models.DateTimeField(auto_now_add=True, help_text=_("Datetime model was created"))
+    updated = models.DateTimeField(auto_now=True, help_text=_("Datetime model was last modified"))
+
+    def age(self):
+        year = timezone.datetime.date(timezone.datetime.now()).year
+        dob_year = self.dob.year
+        return year - dob_year
+    
+    def eoh(self):
+        return timezone.datetime.date(self.doh + timezone.timedelta(days=self.duration*365))
+
+    class Meta:
+        ordering = ("-doh", "-duration")
+        verbose_name = "Detail"
+        verbose_name_plural = "Details"
+    
